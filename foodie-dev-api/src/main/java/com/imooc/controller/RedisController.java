e0f036a10cc8017e222cd72b564f8a2446d80d6f
苗浩 <miaohao@dhcc.com.cn>
2023-03-09 15:30:50 +0800

    第七周2-04 SpringBoot整合,添加redis工具类

--- foodie-dev-api/src/main/java/com/imooc/controller/RedisController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/RedisController.java
@@ -1,5 +1,6 @@
 package com.imooc.controller;
 
+import com.imooc.utils.RedisOperator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,23 +16,29 @@ public class RedisController {
     @Autowired
     private RedisTemplate redisTemplate;
 
+    // 引入redis的工具类
+    @Autowired
+    private RedisOperator redisOperator;
+
     @GetMapping("/set")
     public Object set(String key, String value) {
-        redisTemplate.opsForValue().set(key, value);
+//        redisTemplate.opsForValue().set(key, value);
+        // 调用工具类进行存储
+        redisOperator.set(key,value);
         return "OK";
     }
 
     @GetMapping("/get")
     public String get(String key) {
-        return (String)redisTemplate.opsForValue().get(key);
+//        return (String)redisTemplate.opsForValue().get(key);
+        return redisOperator.get(key);
     }
+
 
     @GetMapping("/delete")
     public Object delete(String key) {
-        redisTemplate.delete(key);
+//        redisTemplate.delete(key);
+        redisOperator.del(key);
         return "OK";
     }
-
-
-
 }
