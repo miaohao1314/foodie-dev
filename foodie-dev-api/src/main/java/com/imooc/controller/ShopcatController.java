f5b7b1af20c7f6bba757cab3225c410fd345d0fd
miaohao <>
2023-03-10 16:27:41 +0800

    第七周，2-07基于redis优化购物车删除数据

--- foodie-dev-api/src/main/java/com/imooc/controller/ShopcatController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/ShopcatController.java
@@ -40,29 +40,30 @@ public class ShopcatController extends BaseController {
         }
 
         // 这里前端直接包装了一个json对象将数据传过来的
-        System.out.println("shopcartBO商品是：  "+shopcartBO);
+        System.out.println("shopcartBO商品是：  " + shopcartBO);
 
         // TODO 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
         String shopcartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
-        List<ShopcartBO>  shopcartList = null;
-        if(StringUtils.isNoneBlank(shopcartJson)){
-            shopcartList =JsonUtils.jsonToList(shopcartJson,ShopcartBO.class);
+        List<ShopcartBO> shopcartList = null;
+        if (StringUtils.isNoneBlank(shopcartJson)) {
+            shopcartList = JsonUtils.jsonToList(shopcartJson, ShopcartBO.class);
             boolean isHaving = false;
+            // 这里判断的是有这个商品id在此基础上添加相对应的值，没有值则不进入for循环
             for (ShopcartBO sc : shopcartList) {
                 String tmpSpecId = sc.getSpecId();
-                if(tmpSpecId.equals(shopcartBO.getSpecId())){
-                    sc.setBuyCounts(sc.getBuyCounts()+shopcartBO.getBuyCounts());
+                if (tmpSpecId.equals(shopcartBO.getSpecId())) {
+                    sc.setBuyCounts(sc.getBuyCounts() + shopcartBO.getBuyCounts());
                     isHaving = true;
                 }
             }
-            if(!isHaving){
+            if (!isHaving) {
                 shopcartList.add(shopcartBO);
             }
-        }else{
+        } else {
             shopcartList = new ArrayList<>();
             shopcartList.add(shopcartBO);
         }
-        redisOperator.set(FOODIE_SHOPCART + ":" +userId, JsonUtils.objectToJson(shopcartList));
+        redisOperator.set(FOODIE_SHOPCART + ":" + userId, JsonUtils.objectToJson(shopcartList));
         return IMOOCJSONResult.ok();
     }
 
@@ -80,7 +81,19 @@ public class ShopcatController extends BaseController {
         }
 
         // TODO 用户在页面删除购物车中的商品数据，如果此时用户已经登录，则需要同步删除后端购物车中的商品
-
+        String shopcartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+        List<ShopcartBO> shopcartList = null;
+        if (StringUtils.isNoneBlank(shopcartJson)) {
+            shopcartList = JsonUtils.jsonToList(shopcartJson, ShopcartBO.class);
+            for (ShopcartBO sc : shopcartList) {
+                String tmpSpecId = sc.getSpecId();
+                if (tmpSpecId.equals(tmpSpecId)) {
+                    shopcartList.remove(sc);
+                    break;
+                }
+            }
+            redisOperator.set(FOODIE_SHOPCART + ":" + userId, JsonUtils.objectToJson(shopcartList));
+        }
         return IMOOCJSONResult.ok();
     }
 }
