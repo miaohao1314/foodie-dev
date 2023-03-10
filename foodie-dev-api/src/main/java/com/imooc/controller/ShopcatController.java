a90c43c26ad329b420225e791e09ea650842b46f
miaohao <>
2023-03-10 16:02:00 +0800

    第七周，2-06基于redis优化购物车

--- foodie-dev-api/src/main/java/com/imooc/controller/ShopcatController.java
+++ foodie-dev-api/src/main/java/com/imooc/controller/ShopcatController.java
@@ -2,19 +2,29 @@ package com.imooc.controller;
 
 import com.imooc.pojo.bo.ShopcartBO;
 import com.imooc.utils.IMOOCJSONResult;
+import com.imooc.utils.JsonUtils;
+import com.imooc.utils.RedisOperator;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.spring.web.json.Json;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 //第四
 @Api(value = "购物车接口controller", tags = {"购物车接口相关的api"})
 @RequestMapping("shopcart")
 @RestController
-public class ShopcatController {
+public class ShopcatController extends BaseController {
+
+    @Resource
+    private RedisOperator redisOperator;
 
     @ApiOperation(value = "添加商品到购物车", notes = "添加商品到购物车", httpMethod = "POST")
     @PostMapping("/add")
@@ -30,10 +40,29 @@ public class ShopcatController {
         }
 
         // 这里前端直接包装了一个json对象将数据传过来的
-        System.out.println(shopcartBO);
+        System.out.println("shopcartBO商品是：  "+shopcartBO);
 
         // TODO 前端用户在登录的情况下，添加商品到购物车，会同时在后端同步购物车到redis缓存
-
+        String shopcartJson = redisOperator.get(FOODIE_SHOPCART + ":" + userId);
+        List<ShopcartBO>  shopcartList = null;
+        if(StringUtils.isNoneBlank(shopcartJson)){
+            shopcartList =JsonUtils.jsonToList(shopcartJson,ShopcartBO.class);
+            boolean isHaving = false;
+            for (ShopcartBO sc : shopcartList) {
+                String tmpSpecId = sc.getSpecId();
+                if(tmpSpecId.equals(shopcartBO.getSpecId())){
+                    sc.setBuyCounts(sc.getBuyCounts()+shopcartBO.getBuyCounts());
+                    isHaving = true;
+                }
+            }
+            if(!isHaving){
+                shopcartList.add(shopcartBO);
+            }
+        }else{
+            shopcartList = new ArrayList<>();
+            shopcartList.add(shopcartBO);
+        }
+        redisOperator.set(FOODIE_SHOPCART + ":" +userId, JsonUtils.objectToJson(shopcartList));
         return IMOOCJSONResult.ok();
     }
 
