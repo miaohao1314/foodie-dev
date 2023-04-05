39fd23234b1bf5430653e12da668d47aa0eb0eaf
miaohao <>
2023-04-05 10:43:03 +0800

    十二周： 2-14：2-14 RabbitMQ与SpringBoot整合_生产端

---
+++ rabbit-producer/src/main/java/com.imooc/Application.java
@@ -0,0 +1,12 @@
+package com.imooc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 代表启动这个程序
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
