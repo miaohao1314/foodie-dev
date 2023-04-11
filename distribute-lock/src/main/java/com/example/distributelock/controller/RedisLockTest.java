4bb2ad0c8cee50dab302596cd101d6d1f60c1191
miaohao <>
2023-04-11 12:14:07 +0800

    14周 2-10  封装redis锁使其成为一个工具类可以调用

---
+++ distribute-lock/src/main/java/com/example/distributelock/controller/RedisLockTest.java
@@ -0,0 +1,41 @@
+package com.example.distributelock.controller;
+
+import com.example.distributelock.util.RedisLock;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.UUID;
+
+@RestController
+@Slf4j
+public class RedisLockTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @RequestMapping("RedisLockTest")
+    public  String redisLock(){
+        log.info("我进入了方法");
+
+        // 设置key
+        String key="redisKey";
+        RedisLock redisLock=new RedisLock(redisTemplate,key,30);
+
+        if(redisLock.getLock()){
+            log.info("我进入了锁");
+            // 模拟一个处理过程15s
+            try {
+                Thread.sleep(15000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }finally {
+                boolean unlock = redisLock.unlock();
+                log.info("释放锁的结果是： "+unlock);
+            }
+        }
+        log.info("方法执行完成");
+        return  "方法执行完成";
+    }
+}
---
+++ distribute-lock/src/main/java/com/example/distributelock/util/RedisLock.java
@@ -0,0 +1,73 @@
+package com.example.distributelock.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.script.RedisScript;
+import org.springframework.data.redis.core.types.Expiration;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+public class RedisLock {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    private  String key;
+    private  String value;
+    // 过期时间，单位秒
+    private  int expireTime;
+
+    public  RedisLock(RedisTemplate redisTemplate,String key,int expireTime){
+        this.redisTemplate=redisTemplate;
+        this.key=key;
+        this.expireTime=expireTime;
+        // 设置value值
+        this.value= UUID.randomUUID().toString();
+    }
+
+    /**\
+     * 获取锁的方法
+     * @return
+     */
+    public  boolean getLock(){
+        RedisCallback<Boolean> redisCallback= redisConnection -> {
+            // 设置NX
+            RedisStringCommands.SetOption setOption=RedisStringCommands.SetOption.ifAbsent();
+            // 过期时间
+            Expiration expiration=Expiration.seconds(expireTime);
+            // 将key值转化为byte类型
+            byte [] redisKey=redisTemplate.getKeySerializer().serialize(key);
+            // 序列化value
+            byte [] redisValue = redisTemplate.getValueSerializer().serialize(value);
+            // 执行setnx操作
+            Boolean result = redisConnection.set(redisKey,redisValue,expiration,setOption);
+            return  result;
+        };
+        // 获取分布式锁
+        Boolean lock = (Boolean) redisTemplate.execute(redisCallback);
+        return  lock;
+    }
+
+
+    /**
+     * 释放锁
+     * @return
+     */
+    public  boolean unlock(){
+        // 释放锁定过程使用lua脚本
+        String script=" if redis.call(\"get\",KEYS[1]) == ARGV[1] then\n" +
+                "                 return redis.call(\"del\",KEYS[1])\n" +
+                "                 else\n" +
+                "                 return 0\n" +
+                "                 end";
+        RedisScript<Boolean> redisScript=RedisScript.of(script,Boolean.class);
+        List<String> keys = Arrays.asList(key);
+
+        Boolean  result = (Boolean) redisTemplate.execute(redisScript, keys, value);
+        return  result;
+    }
+}
