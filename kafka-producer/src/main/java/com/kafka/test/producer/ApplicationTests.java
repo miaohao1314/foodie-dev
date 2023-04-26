cad5d26337800a648eb33ebca13b545e1999237f
miaohao <>
2023-04-26 15:06:02 +0800

    13 周 1-6  kafka复习（发送消息）

--- kafka-producer/src/main/java/com/kafka/test/producer/ApplicationTests.java
+++ kafka-producer/src/main/java/com/kafka/test/producer/ApplicationTests.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ * 第三个：
+ * 发送消息测试类,往miaohao-topic里面去发送消息
+ */
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
@@ -20,7 +24,7 @@ public class ApplicationTests {
 
         String topic = "miaohao-topic";
         for(int i=0; i < 10; i ++) {
-            kafkaProducerService.sendMessage(topic, "hello kafka123" + i);
+            kafkaProducerService.sendMessage(topic, "hello kafka123_miaohao" + i);
             Thread.sleep(5);
         }
         Thread.sleep(Integer.MAX_VALUE);
