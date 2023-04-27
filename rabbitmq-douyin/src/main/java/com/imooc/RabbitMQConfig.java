618b82299194d5d3e5795da80f7b97c9cdf0b455
miaohao <>
2023-04-27 12:32:47 +0800

    12周 rabbitmq新学复习(创建生产者，配置路由规则)

--- rabbitmq-douyin/src/main/java/com/imooc/RabbitMQConfig.java
+++ rabbitmq-douyin/src/main/java/com/imooc/RabbitMQConfig.java
@@ -19,28 +19,46 @@ public class RabbitMQConfig {
 
 //    ctrl+shift+u: 转为大写
     // 1，定义交换机
-    public  static  final  String  EXCHANGE_MST="exchange_mst";
+    public static final String EXCHANGE_MST = "exchange_mst";
 
     // 2，定义队列
-    public  static  final  String QUEUE_SYS_MSG="queue_sys_msg";
+    public static final String QUEUE_SYS_MSG = "queue_sys_msg";
 
     // 3，创建交换机, 要将交换机放置到springboot中，需要用到bean这个注解
     @Bean(EXCHANGE_MST)
-    public Exchange exchange(){
+    public Exchange exchange() {
         // 构建交换机，使用topic类型,设置持久化durable(true)，重启后依然存在
         return ExchangeBuilder.topicExchange(EXCHANGE_MST).durable(true).build();
     }
 
     //4，创建队列
     @Bean(QUEUE_SYS_MSG)
-    public Queue queue(){
-        return new Queue(QUEUE_SYS_MSG,true);
+    public Queue queue() {
+        return new Queue(QUEUE_SYS_MSG, true);
     }
+
+
+    /**
+     * 路由规则
+     * * 代表一个占位符
+     * sys.*.*
+     * sys.a.b  :可以匹配
+     * sys.a.b.c  :不可以匹配
+     *
+     * #代表多个占位符
+     * sys.#
+     * 都可以匹配
+     * sys.a.b
+     *sys.a.b.c
+     */
 
     //5，队列和交换机的绑定
     @Bean
-    public Binding binding(@Qualifier(EXCHANGE_MST) Exchange  exchange, @Qualifier(QUEUE_SYS_MSG) Queue queue){
+    public Binding binding(@Qualifier(EXCHANGE_MST) Exchange exchange, @Qualifier(QUEUE_SYS_MSG) Queue queue) {
         // 使用在容器中的这两个对象,with代表routingkey(定义路由规则，类似于requestMapping)
         return BindingBuilder.bind(queue).to(exchange).with("sys.msg.*").noargs();
     }
+
+
+
 }
---
+++ rabbitmq-douyin/src/main/java/com/imooc/controller/HelloController.java
@@ -0,0 +1,28 @@
+package com.imooc.controller;
+
+
+import com.imooc.RabbitMQConfig;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * rabbitmq生产者发送消息
+ */
+
+@RestController
+@RequestMapping("hello")
+public class HelloController {
+
+    //    Springboot和中间件结合，为模板类
+    @Autowired
+    public RabbitTemplate rabbitTemplate;
+
+    @GetMapping("produce")
+    public Object produce() throws Exception {
+        rabbitTemplate.convertAndSend(RabbitMQConfig.EXCHANGE_MST, "sys.msg.send", "我发了一个消息");
+        return "success";
+    }
+}
